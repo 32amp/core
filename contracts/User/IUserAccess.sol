cc6d60b300706eb4d2a55fd079aa27f5f37edb95
Mikhail <spiritkhl@gmail.com>
2024-08-30 10:48:41 +0300

    update IUserAccess interface

--- contracts/User/IUserAccess.sol
+++ contracts/User/IUserAccess.sol
@@ -5,16 +5,16 @@ interface IUserAccess {
 
     // ZERO - Access denied
     // FIRST - Only view
-    // SECOND - view and edit
-    // THIRD - view, edit, add
-    // FOURTH - view, edit, add, delete
+    // SECOND - View and Run
+    // THIRD - View, Run, Edit
+    // FOURTH - View, Edit, Add
+    // FIFTH - View, Edit, Add, Delete
+    // GOD - GODMODE
 
     enum  AccessLevel{ZERO, FIRST, SECOND, THIRD, FOURTH, FIFTH, GOD} 
 
     function setAccessLevelToModule(bytes32 _token, uint256 user_id, string memory module, AccessLevel access_level) external;
-
     function getModuleAccessLevel(string memory module, uint256 user_id) external view returns(uint);
-
     function getObjectAccessLevel(string memory module, bytes32 object_id, uint256 user_id) external view returns(uint);
     function setAccessLevelToModuleObject(bytes32 _token, bytes32 object_id, uint256 user_id, string memory module, AccessLevel access_level) external;
     function getGroupModuleAccessLevel(string memory module, uint256 group_id) external view returns(uint);
